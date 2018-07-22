e97badfb9d21b0b20d262892d6f9e79afbf81bb9
Petr Mrázek <peterix@gmail.com>
2018-07-23 00:49:26 +0200

    SCRATCH things and stuff, related to grou saving

--- application/MainWindow.cpp
+++ application/MainWindow.cpp
@@ -1609,7 +1609,7 @@ void MainWindow::on_actionDeleteInstance_triggered()
     )->exec();
     if (response == QMessageBox::Yes)
     {
-        m_selectedInstance->nuke();
+        MMC->instances()->deleteInstance(m_selectedInstance->id());
     }
 }
 
